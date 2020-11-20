bfc864f2960df1d1ee44affa74fa49b490345204
qvqstarry <907452780@qq.com>
2020-11-20 15:04:29 +0200

    .

--- Contribute.js
+++ Contribute.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  $("#button_register").hover(function() {
+    $(this).css("background", "#559900");
+  }, function() {
+    $(this).css("background", "#9CDA1E");
+  });
+  
   $(".change_btn").hover(function(){
     $(this).css("background","#161513");
     document.getElementById("Choose_Pic").style.color = "#FFFFFF";
